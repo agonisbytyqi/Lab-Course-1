f9d5da4ab4de94626822590c51c0d6a52e4c76ea
agonisbytyqi <bytyqiagonis@gmail.com>
2021-05-04 01:57:38 +0200

    TableToHoldUsersDetails

---
+++ TableToHoldUsersDetails.sql
@@ -0,0 +1,13 @@
+use ParkingHero
+
+create table Users (
+FirtName varchar(50),
+LastName varchar(50),
+UserName varchar(50),
+Password varchar(50),
+Email varchar(50),
+PhoneNumber int
+)
+
+select * from Users
+insert into Users values ('Agonis','Bytyqi','agonisbytyqi','0011','ab50615@ubt-uni.net',044123456)
